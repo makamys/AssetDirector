f6176d159f679e0559975d741d1d24db0f595607
makamys <makamys@outlook.com>
2021-10-20 16:52:01 +0200

    Init project

--- src/main/java/makamys/assetdirector/Assetdirector.java
+++ src/main/java/makamys/assetdirector/Assetdirector.java
@@ -1,14 +1,14 @@
-package ${package};
+package makamys.assetdirector;
 
 import net.minecraft.init.Blocks;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.EventHandler;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 
-@Mod(modid = ${mainclass}.MODID, version = ${mainclass}.VERSION)
-public class ${mainclass}
+@Mod(modid = Assetdirector.MODID, version = Assetdirector.VERSION)
+public class Assetdirector
 {
-    public static final String MODID = "${modid}";
+    public static final String MODID = "assetdirector";
     public static final String VERSION = "@VERSION@";
 
     @EventHandler
